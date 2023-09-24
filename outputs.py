cfce859db63db2cc2440de32f6127fcb7b170408
naidile05 <naidilemurali@Naidiles-MacBook-Air.local>
2023-09-24 19:34:11 -0400

    output file changes

--- outputs.py
+++ outputs.py
@@ -17,6 +17,17 @@ def display_outputs(outputs):
 
     #Dummy Output - Start
     st.write(f"The Inputs are: \n\n product_name: {outputs['product_name']} \n\n credit_cards_owned: {outputs['credit_cards_owned']} \n\n price_range_lower: {outputs['price_range_lower']} \n\n price_range_upper: {outputs['price_range_upper']}")
+    col1, col2 = st.columns(2)
+
+    # Insert product 1 details in the first column
+    with col1:
+        st.image('product1.jpg', use_column_width=True)
+        st.write('Price: $19.99')
+
+    # Insert product 2 details in the second column
+    with col2:
+        st.image('product2.jpg', use_column_width=True)
+        st.write('Price: $24.99')
     #Dummy Output - End
 
 
